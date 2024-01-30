b23a73f3e5e4f1d9f8c161f6b88da92ad099874e
ecemchen <ecemtasali@gmail.com>
2024-01-31 00:46:00 +0100

    cant find why does cartman not appear in different holes

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const cursor = document.querySelector('.cursor') // to fix the hammer image to the cursor: defined a variable cursor and assigned it to the class .cursor in the html file 
+const holes = [document.querySelectorAll('.hole')] // so that i dont need to put img src to every hole div in html, i make an array of all the holes
+
+function run() {
+    const i = Math.floor(Math.random() * holes.length) // to get a random number between 0 and 8 (holes.length)
+    const hole = holes[i]
+
+    const img = document.createElement('img')
+    img.classList.add('cartman')
+    img.src = 'assets/cartman.png'
+
+    hole.appendChild(img)
+}
+run()
 
 window.addEventListener('mousemove', e => { //added an event listener here so every time mouse moves it will call the function e and func e defined below)
     cursor.style.top = e.pageY + 'px'
@@ -10,14 +23,14 @@ window.addEventListener('mousemove', e => { //added an event listener here so ev
 //////////////// ASK ABOUT THIS: What about the right click, I found this https://javascript.plainenglish.io/how-to-handle-right-clicks-with-javascript-86d35714ce33 //////////////////////////
 
 window.addEventListener('mousedown', () => { //added an event listener here so every time mouse clicks it will call the function e and func e defined below)
-    cursor.classList.add('active') 
+    cursor.classList.add('active')
 })
 
 //So this is when we click and button goes down, hammer rotates but it stays like that so we should add another event listener for making the hammer straight again when we release the mouse
 
-window.addEventListener('mouseup', () => { 
-     cursor.classList.remove('active') 
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
 })
 
-// NEXT STEP: ADDING THE RANDOMLY CREATED MOLES 
+// NEXT STEP: ADDING THE RANDOMLY CREATED MOLES
 //SO GO TO HTML AND ADD THE CARTMAN_HITLER_PNG after the 1st div class hole
